8b962e99240089e1a3f706e12d2b9c01e5e20d88
matt <m@matt.tv>
2023-08-01 21:24:43 -0400

    updates to outputfiles.py

--- outputfiles.py
+++ outputfiles.py
@@ -6,47 +6,50 @@ import urllib.request
 import base64
 import time
 import os
+import datetime
+
+def list_files_in_dir(directory):
+    print(f'{"File Name":<50} {"File Size":<10} {"Last Modified"}')
+    print('-' * 80)    
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        size = os.path.getsize(filepath)
+        modified_time = os.path.getmtime(filepath)
+        modified_time = datetime.datetime.fromtimestamp(modified_time).strftime('%Y-%m-%d %H:%M:%S')
+        print(f'{filename:<50} {size:<10} {modified_time}')
+
+print("\nThis script will write files to /referencefiles.  Initially, the directory is empty.")
+print("")
+list_files_in_dir("referencefiles")
+print("")
 
 encoding = tiktoken.get_encoding("cl100k_base")  # Using the cl100k_base model.
 
-def set_tiktoken_encoding():
-    """
-    We'll set the tiktoken model as 'cl100k_base model' since that model is used for GPT-4.
-    
-    # File:  cl100k_base.tiktoken
-    
-    It includes the dictionary of base64 encoded tokens and their index numbers.
-
-    The source file is at this URL:  https://openaipublic.blob.core.windows.net/encodings/cl100k_base.tiktoken
-    """
-
-    
-print(set_tiktoken_encoding.__doc__); time.sleep(1)
+print("We'll set the tiktoken model as 'cl100k_base model' since that model is used for GPT-4.")
+print("Next, this will write the raw tiktoken dictionary file which consists of base64 encoded tokens and their index numbers.")
+print("The source file is at this URL:  https://openaipublic.blob.core.windows.net/encodings/cl100k_base.tiktoken")
 
 url = "https://openaipublic.blob.core.windows.net/encodings/cl100k_base.tiktoken"
 file_path = "referencefiles/cl100k_base.tiktoken"
 
-try:
-    local_file, headers = urllib.request.urlretrieve(url, file_path)
-    print("\nList of files saved in " , os.getcwd() ,"...\n", os.listdir("referencefiles"), "\n")
-except Exception as e:
-    print(f"An error occurred: {e}")
+urllib.request.urlretrieve(url, file_path)
 
-def write_tiktoken_byteliteral_utf8string(): 
-    """
-    # Files:  cl100k_base_byteliterals.txt, cl100k_base_utf8.txt
+print("")
+list_files_in_dir("referencefiles")
+print("")
+
     
-    This step will convert the text-related values in the cl100k_base file and output two additional files.
+print("This step will convert the text-related values in the cl100k_base file and output two additional files.")
 
-    For the first output file, we'll be decoding the bash64 values which will result in byte literals.  
-    For example, the letter A becomes the string literal b'A'.
+print("For the next output file, the bash64 values will be decoded to display the byte literals.  ")
+print("For example, the letter A becomes the string literal b'A'.")
 
-    For the second output file, we'll decode the base64 values into UTF-8 strings so that we can see double-byte characters.
-        For example, character 22656 is the byte literal b'\xe6\x9c\xac' which corresponds to the Japanese kanji 本.
-        Sometimes the bytes are not valid UTF-8 so it substitutes the default 'replace' unicode character.
-        Sometimes there will be formatting characters like carriage returns that will display weirdness in a text editor.
-        cl100k_base_byteliterals.txt and cl100k_base_utf8.txt have been written successfully.
-    """
+print("For the next output file, the base64 values will be decoded into UTF-8 strings in order to see double-byte characters.")
+print("  For example, character 22656 is the byte literal b'\xe6\x9c\xac' which corresponds to the Japanese kanji 本.")
+print("  Sometimes the bytes are not valid UTF-8 so it substitutes the default 'replace' unicode character.")
+print("  Sometimes there will be formatting characters like carriage returns that will display weirdness in a text editor.")
+print("  cl100k_base_byteliterals.txt and cl100k_base_utf8.txt have been written successfully.")
+
 with open('referencefiles/cl100k_base.tiktoken', 'r') as infile, \
      open('referencefiles/cl100k_base_byteliterals.txt', 'w') as outfile_bytes, \
      open('referencefiles/cl100k_base_utf8.txt', 'w') as outfile_utf8:
@@ -56,5 +59,7 @@ with open('referencefiles/cl100k_base.tiktoken', 'r') as infile, \
         outfile_bytes.write(f"{number} {decoded_bytes}\n")  # Write the byte string literal to the first output file
         decoded_string = decoded_bytes.decode('utf-8', errors='replace')  
         outfile_utf8.write(f"{number} {decoded_string}\n")  # Write the UTF-8 string to the second output file
-print(write_tiktoken_byteliteral_utf8string.__doc__); time.sleep(1)
-print("\nList of files saved in " , os.getcwd() ,"...\n", os.listdir("referencefiles"), "\n")
+
+print("")
+list_files_in_dir("referencefiles")
+print("")
